60a2c653326efb745a4f9571cb2b8960a3534d8d
Olabisi Odusanya <odus.bisi07@gmail.com>
2022-05-14 22:17:39 +0300

    Feat: Complete extract, transform and validation steps

--- spoony.py
+++ spoony.py
@@ -1,9 +1,44 @@
-import spoonacular as sp
-import json
+import requests
+import pandas as pd
+
 from api_keys import spoon_api
+from validation_helper import check_if_valid_data
 
-my_api = sp.api(spoon_api())
 
-resp = my_api.parse_ingredients("3.5 cups King Arthur flour", servings=2)
-data = resp.json()
-print(data[0]['name'])
+def extract_data():
+    # fill your spoonacular api key
+    api_key = spoon_api()
+
+    # get some sample endpoints from spoonacular
+    url = f'https://api.spoonacular.com/recipes/715538/similar?apiKey={api_key}'
+    url2 = f'https://api.spoonacular.com/recipes/complexSearch?apiKey={api_key}&query=pasta&maxFat=25&number=20'
+    response = requests.get(url2)
+    response_data = response.json()
+
+    # create empty list for your data
+    title = []
+    nutrition = []
+
+    for r in response_data['results']:
+        title.append(r['title'])
+        nutrition.append(r['nutrition']['nutrients'][0]['amount'])
+
+    # Put data in dataframe (Transform)
+    pasta_dict = {
+        "title": title,
+        "nutrition": nutrition
+    }
+
+    pasta_df = pd.DataFrame(pasta_dict, columns=["title", "nutrition"])
+
+    # Validate
+    if check_if_valid_data(pasta_df):
+        print('Data works fine, You can load it!')
+        return pasta_df
+
+    # Load data
+
+    # Schedule jobs
+
+
+print(extract_data())
